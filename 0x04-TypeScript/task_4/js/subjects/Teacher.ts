8e744d3364a7bcb131de9520ee39748abe76bd60
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2023-10-07 10:52:03 -0700

    Task 9

---
+++ 0x04-TypeScript/task_4/js/subjects/Teacher.ts
@@ -0,0 +1,10 @@
+// js/subjects/Teacher.ts
+namespace Subjects {
+  export interface Teacher {
+    firstName: string;
+    lastName: string;
+    experienceTeachingC?: number;
+    experienceTeachingReact?: number;
+    experienceTeachingJava?: number;
+  }
+}
